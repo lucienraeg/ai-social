0b09d4ec4eb1e3aa8e4d009b040ef781ad3f372a
lucienraeg <lucienraegentil@gmail.com>
2018-04-29 21:54:59 +1000

    refine CSS styling on posts

--- app/routes.py
+++ app/routes.py
@@ -13,10 +13,12 @@ def feed():
     posts = [
         {
             'author': {'username': 'jeff'},
+            'date': '8:25 AM 11-7-18',
             'body': 'I like the number seven.'
         },
         {
             'author': {'username': 'susan'},
+            'date': '11:30 PM 21-8-18',
             'body': 'Weetbix is pretty great! Here are some more words... yes!\
             El gobierno quiere comer todos los gatos.'
         }
